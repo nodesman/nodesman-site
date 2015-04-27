3ee394873c5e0de7167c15e73d13d6a5e8bb8ba3
Rajs <flarecore@gmail.com>
2015-04-28 01:12:00 +0530

    WIP implementing pages add.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,22 @@
 class AdminController < ApplicationController
+  attr_writer :title, :content, :page_type, :content_type, :url_slug
   layout "admin"
+
   def home
     render 'admin-home'
   end
 
   def edit_page
 
+  end
+
+  def add_page_post
+    new_page = Page.create(new_post_params)
+    redirect_to '/admin/pages'
+  end
+
+  def new_post_params
+    params.require(:page).permit(:title, :content, :page_type, :content_type, :url_slug)
   end
 
   def contact_info
@@ -17,7 +28,7 @@ class AdminController < ApplicationController
   end
 
   def pages
-
+    @pages = Page.all()
   end
 
   def add_page

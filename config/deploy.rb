4816d7e2a48f0da2036281e7b9920cfe6f9a579c
Rajs <flarecore@gmail.com>
2015-05-10 17:56:03 +0530

    Updated cap to restart passenger with sudo

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, '/home/raj/nodesman'
 
 # Default value for :scm is :git
 set :scm, :git
-
+set :passenger_restart_with_sudo, true
 # Default value for :format is :pretty
 # set :format, :pretty
 

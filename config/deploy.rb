bb71f4652602be8a20d1e9c5c1f9e45ada8d64df
Rajs <flarecore@gmail.com>
2015-05-15 00:12:43 +0530

    Set default stage.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 
 
 set :application, 'nodesman'
-set :stage, :production
+
 set :repo_url, 'git@bitbucket.org:nodesman/nodesman.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

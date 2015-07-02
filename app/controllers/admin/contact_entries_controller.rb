139f399a81c72c1c56797b81dfbef18b64bdffa9
Rajs <flarecore@gmail.com>
2015-07-02 17:04:20 +0530

    Implemented contact form index and show for admin page.

--- app/controllers/admin/contact_entries_controller.rb
+++ app/controllers/admin/contact_entries_controller.rb
@@ -1,4 +1,9 @@
-class Admin::ContactEntriesController
+class Admin::ContactEntriesController < AdminPageController
   def index
+    @entries = ContactFormEntry.order(:created_at).all
+  end
+  def show
+    id = params[:id]
+    @entry = ContactFormEntry.find(id)
   end
 end

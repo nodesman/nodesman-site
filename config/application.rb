86d8fbd8900989e83c9cea803d9759b596f8da61
Rajs <flarecore@gmail.com>
2015-11-25 18:53:50 +0530

    Moved require statement back to app config

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-require "#{Rails.root}/config/initializers/bower_rails.rb"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module NodesmanSite
   class Application < Rails::Application
-
+    require "#{Rails.root}/config/initializers/bower_rails.rb"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

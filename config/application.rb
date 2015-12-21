0b2884593b69678f7bded3509b82a07eec9e799c
Rajs <flarecore@gmail.com>
2015-12-22 03:22:19 +0530

    Adding a new blog post

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,7 +20,6 @@ module NodesmanSite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile = %w( *.sass *.js *.png *.jpg *.gif )
-    config.assets.paths << "#{Rails.root}/app/assets/views/templates"
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end

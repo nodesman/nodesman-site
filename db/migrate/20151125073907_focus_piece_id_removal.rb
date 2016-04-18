4aab74a3ab25eed4f7f561d9ecf94c2e125eaafb
Raj <flarecore@gmail.com>
2016-04-18 22:02:18 +0530

    Updated schema

--- db/migrate/20151125073907_focus_piece_id_removal.rb
+++
@@ -1,5 +0,0 @@
-class FocusPieceIdRemoval < ActiveRecord::Migration
-  def change
-    remove_column :articles, :focus_piece_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125073907) do
+ActiveRecord::Schema.define(version: 20160418162118) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
+    t.boolean  "show_date",   limit: 1,     default: false
     t.string   "title",       limit: 255
     t.text     "content",     limit: 65535
-    t.boolean  "show_date",   limit: 1,     default: false
     t.string   "url_slug",    limit: 255
     t.text     "html",        limit: 65535
     t.boolean  "focus_piece", limit: 1,     default: false
